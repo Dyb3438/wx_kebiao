d6480eb743973e6c78ce67d84e01e0eb525bbd23
Dyb 3438 <503553364@qq.com>
2018-04-26 14:19:11 +0800

    增加操作参数判断，可用于绑定身份

--- src/kebiao.php
+++ src/kebiao.php
@@ -18,6 +18,7 @@ $xh = $_POST['xh'];
 $pw=$_POST['pw'];
 $code=$_POST['txtSecretCode'];
 $rnd=$_POST['rnd'];
+$do=$_POST['do'];//0: 绑定;1: 爬课表;2: 绑定且爬课表;
 
 $_SESSION['xh'] =$xh;
 //登录前获取隐藏字段
@@ -47,81 +48,87 @@ if ($error[1] != null) {
 //爬去学生名字
 $xm_pattern = '/<span id="xhxm">([^<]+)<\/span>/';
 preg_match_all($xm_pattern, $index, $xm);
-if (isset($xm[1][0])) {
+if (isset($xm[1][0])){
+    //进入课表页面
     $xm = substr($xm[1][0], 0, -4);
     $kebiao = $curl->login_curl("http://$rnd/xskbcx.aspx?xh=$xh&xm=$xm&gnmkdm=N121603",$rnd);
-    //取出信息栏
-    $information_pattern="/<span id=\"Label5\">(.+)<\/span>|<span id=\"Label6\">(.+)<\/span>|<span id=\"Label7\">(.+)<\/span>|<span id=\"Label8\">(.+)<\/span>|<span id=\"Label9\">(.+)<\/span>/";
-    preg_match_all($information_pattern,$kebiao,$information);
-    foreach($information[0] as $key=>$value){
-        $information[0][$key]=iconv("gb2312","utf-8",$value);
-    }
-    preg_match_all("/学号：(.+)<\/span>/u",$information[0][0],$xuehao);
-    $xuehao=$xuehao[1][0];
-    preg_match_all("/姓名：(.+)<\/span>/u",$information[0][1],$xingming);
-    $xingming=$xingming[1][0];
-    preg_match_all("/学院：(.+)<\/span>/u",$information[0][2],$xueyuan);
-    $xueyuan=$xueyuan[1][0];
-    preg_match_all("/专业：(.+)<\/span>/u",$information[0][3],$zhuanye);
-    $zhuanye=$zhuanye[1][0];
-    preg_match_all("/行政班：(.+)<\/span>/u",$information[0][4],$xingzhengban);
-    $xingzhengban=$xingzhengban[1][0];
-    //爬取课表
-    $table_pattern = "/<tr>[\s\S]+?<\/tr>/";
-    preg_match_all($table_pattern, $kebiao, $table);
-    $tbody = $table[0];
-    $td_pattern = "/<td( align=\"([^\"]+)\")?( rowspan=\"([^\"]+)\")?( colspan=\"([^\"]+)\")?[^>]*>([\s\S]+?)<\/td>/";
-    $content = array();
-    foreach ($tbody as $i => $value) {
-        preg_match_all($td_pattern, $value, $td);
-        foreach ($td[7] as $a => $class) {
-            for ($check = 0; isset($content[$i][$check]); $check++) {
-            }
-            if ($td[6][$a] != null) {
-                for ($c = 0; $c < $td[6][$a]; $c++) {
-                    if ($c == 0) {
-                        $content[$i][$check] = iconv('gb2312', 'utf-8', $td[7][$a]);
-                    } else {
-                        $content[$i][$check + $c] = "0";
-                    }
-                }
-            } elseif ($td[4][$a] != null) {
-                for ($c = 0; $c < $td[4][$a]; $c++) {
-                    if ($c == 0) {
-                        $content[$i][$check] = iconv('gb2312', 'utf-8', $td[7][$a]);
-                    } else {
-                        $content[$i + $c][$check] = "0";
-                    }
-                }
-            } else {
-                $content[$i][$check] = iconv('gb2312', 'utf-8', $td[7][$a]);
-            }
+    if($do=="0"||$do=="2") {
+        //取出信息栏
+        $information_pattern = "/<span id=\"Label5\">(.+)<\/span>|<span id=\"Label6\">(.+)<\/span>|<span id=\"Label7\">(.+)<\/span>|<span id=\"Label8\">(.+)<\/span>|<span id=\"Label9\">(.+)<\/span>/";
+        preg_match_all($information_pattern, $kebiao, $information);
+        foreach ($information[0] as $key => $value) {
+            $information[0][$key] = iconv("gb2312", "utf-8", $value);
         }
+        preg_match_all("/学号：(.+)<\/span>/u", $information[0][0], $xuehao);
+        $xuehao = $xuehao[1][0];
+        preg_match_all("/姓名：(.+)<\/span>/u", $information[0][1], $xingming);
+        $xingming = $xingming[1][0];
+        preg_match_all("/学院：(.+)<\/span>/u", $information[0][2], $xueyuan);
+        $xueyuan = $xueyuan[1][0];
+        preg_match_all("/专业：(.+)<\/span>/u", $information[0][3], $zhuanye);
+        $zhuanye = $zhuanye[1][0];
+        preg_match_all("/行政班：(.+)<\/span>/u", $information[0][4], $xingzhengban);
+        $xingzhengban = $xingzhengban[1][0];
+        $information=array("xuehao"=>$xuehao,"xingming"=>$xingming,"xueyuan"=>$xueyuan,"zhuanye"=>$zhuanye,"xingzhengban"=>$xingzhengban);
     }
-    $class_list = array();
-    for ($row = 2; $row < count($content); $row++) {
-        for ($list = 2; $list < count($content[$row]); $list++) {
-            if ($content[$row][$list] != "0" && $content[$row][$list] != "&nbsp;") {
-                $pattern = "/([^>]+)<br>周(.)第([^节]+)节{第([^周]+)周(\|([单|双])周)?}<br>([^<]+)<br>([^<]*)/u";
-                preg_match_all($pattern, $content[$row][$list], $output);
-                foreach ($output[0] as $key => $value) {
-                    $output = $data_restore->restore($output, $key);
-                    $class = array(
-                        "classname" => $output[1][$key],
-                        "day" => $output[2][$key],
-                        "class" => $output[3][$key],
-                        "long" => $output[4][$key],
-                        "teacher" => $output[7][$key],
-                        "classroom" => $output[8][$key],
-                        "single_week" => $output[6][$key]
-                    );
-                    array_push($class_list, $class);
+    if($do=="1"||$do=="2") {
+        //爬取课表
+        $table_pattern = "/<tr>[\s\S]+?<\/tr>/";
+        preg_match_all($table_pattern, $kebiao, $table);
+        $tbody = $table[0];
+        $td_pattern = "/<td( align=\"([^\"]+)\")?( rowspan=\"([^\"]+)\")?( colspan=\"([^\"]+)\")?[^>]*>([\s\S]+?)<\/td>/";
+        $content = array();
+        foreach ($tbody as $i => $value) {
+            preg_match_all($td_pattern, $value, $td);
+            foreach ($td[7] as $a => $class) {
+                for ($check = 0; isset($content[$i][$check]); $check++) {
+                }
+                if ($td[6][$a] != null) {
+                    for ($c = 0; $c < $td[6][$a]; $c++) {
+                        if ($c == 0) {
+                            $content[$i][$check] = iconv('gb2312', 'utf-8', $td[7][$a]);
+                        } else {
+                            $content[$i][$check + $c] = "0";
+                        }
+                    }
+                } elseif ($td[4][$a] != null) {
+                    for ($c = 0; $c < $td[4][$a]; $c++) {
+                        if ($c == 0) {
+                            $content[$i][$check] = iconv('gb2312', 'utf-8', $td[7][$a]);
+                        } else {
+                            $content[$i + $c][$check] = "0";
+                        }
+                    }
+                } else {
+                    $content[$i][$check] = iconv('gb2312', 'utf-8', $td[7][$a]);
                 }
             }
         }
+        $class_list = array();
+        for ($row = 2; $row < count($content); $row++) {
+            for ($list = 2; $list < count($content[$row]); $list++) {
+                if ($content[$row][$list] != "0" && $content[$row][$list] != "&nbsp;") {
+                    $pattern = "/([^>]+)<br>周(.)第([^节]+)节{第([^周]+)周(\|([单|双])周)?}<br>([^<]+)<br>([^<]*)/u";
+                    preg_match_all($pattern, $content[$row][$list], $output);
+                    foreach ($output[0] as $key => $value) {
+                        $output = $data_restore->restore($output, $key);
+                        $class = array(
+                            "classname" => $output[1][$key],
+                            "day" => $output[2][$key],
+                            "class" => $output[3][$key],
+                            "long" => $output[4][$key],
+                            "teacher" => $output[7][$key],
+                            "classroom" => $output[8][$key],
+                            "single_week" => $output[6][$key]
+                        );
+                        array_push($class_list, $class);
+                    }
+                }
+            }
+        }
+        $class_list = $data_restore->combine_class($class_list);
     }
-    $class_list=$data_restore->combine_class($class_list);
-    echo json_encode(array("result"=>"1","kebiao"=>$class_list,"information"=>array("xuehao"=>$xuehao,"xingming"=>$xingming,"xueyuan"=>$xueyuan,"zhuanye"=>$zhuanye,"xingzhengban"=>$xingzhengban),"week"=>$week->get_week()));
+    echo json_encode(array("result"=>"1","kebiao"=>isset($class_list)?$class_list:"","information"=>isset($information)?$information:"","week"=>$week->get_week()));
 }else{
     echo json_encode(array("result"=>"0","msg"=>$error[1][0]));
 }
